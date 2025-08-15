5f76b45a8e3a65ffc9bdbacc7027b9cebdc1c055
kayque <kayquec158@gmail.com>
2025-08-15 16:57:33 -0300

    Funções em PHP - 15.08.2025

---
+++ Aula 03/introducao.php
@@ -0,0 +1,25 @@
+<?php
+
+    $modelo_carro1="varsar";
+    $marca_carro1="nissan";
+    $ano_carro1 = 2020;
+    $revisao_carro1= true;
+
+    $modelo_carro1="varsar";
+    $marca_carro1="nissan";
+    $ano_carro1 = 2020;
+    $revisao_carro1= true;
+
+    $modelo_carro1="varsar";
+    $marca_carro1="nissan";
+    $ano_carro1 = 2020;
+    $revisao_carro1= true;
+
+    $modelo_carro1="varsar";
+    $marca_carro1="nissan";
+    $ano_carro1 = 2020;
+    $revisao_carro1= true;
+
+    function () {
+
+    }
