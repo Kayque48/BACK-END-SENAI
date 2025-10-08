a60e13727b80d33186b7a49c549d51acb1f774be
kayque <kayquec158@gmail.com>
2025-10-08 16:06:18 -0300

    test: práticando POO com foco em desenvolvimento de classes e métodos

---
+++ AULA11/Herois.php
@@ -0,0 +1,54 @@
+<?php
+
+    interface Local{
+        public function Dever();
+    }
+
+    class Estrutura{
+        private $heroi;
+        
+        public function __construct($heroi) {
+            $this->setHeroi($heroi);
+        }
+
+        public function setHeroi($heroi) {
+            $this->heroi = $heroi;
+        }
+
+        public function getHeroi() {
+            return $this->heroi;
+        }
+    }
+
+    class Cotil extends Estrutura implements Local{
+        public function __construct($heroi) {
+            parent:: __construct($heroi);
+        }
+
+        public function Dever(){
+            echo "O {$this->getheroi()} está treinando no Cotil nesse momento\n";
+        }
+    }
+
+    class Shopping extends Estrutura implements Local{
+        public function __construct($heroi) {
+            parent:: __construct($heroi);
+        }
+
+        public function Dever() {
+            echo "O {$this->getHeroi()} após terminar seu treinamento no Cotil, está nesse momento, realizando Doação no Shopping.\n";
+        }
+    }
+
+    $herois = [
+        new Cotil('Batman'),
+        new Cotil('Superman'),
+        new Cotil('Homen-Aranha'),
+        new Shopping('Batman'),
+        new Shopping('Superman'),
+        new Shopping('Homen-Aranha')
+    ];
+
+   foreach($herois as $heroi) {
+        echo $heroi->Dever();
+    }
